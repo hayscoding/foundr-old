b0ba65a860d3c3af979128a02b3bebf67272e059
Hays <stanfordhays@gmail.com>
2017-01-12 18:27:16 -0600

    Other user profiles are now queried by firebase. picture is shown on home screen, still working on functionality.

--- app/modules/firebaseAPI.js
+++ app/modules/firebaseAPI.js
@@ -47,8 +47,101 @@ export const mergeUser = (uid, newData) => {
   })  
 }
 
+export const unlikeProfile = (userUid, profileUid) => {
+  firebase.database().ref().child('relationships').child(userUid).child('liked')
+  .child(profileUid).set(false)
+}
+
+export const checkMatch = (userUid, profileUid, func) => {
+  firebase.database().ref().child('relationships').child(userUid).child('likedBack')
+  .child(profileUid).once('value', (snap) => func(snap.val()))
+}
+
+export const likeProfile = (userUid, profileUid) => {
+  firebase.database().ref().child('relationships').child(userUid).child('liked')
+    .child(profileUid).set(true)
+  firebase.database().ref().child('relationships').child(profileUid).child('likedBack')
+  .child(userUid).set(true)
+}
 
 export const getUser = (key) => {
   return firebase.database().ref().child('users').child(key).once('value')
     .then((snap) => snap.val())
+}
+
+export const watchUser = (key, func) => {
+  firebase.database().ref().child('users/'+key).on('value', (snap) => {
+    func(snap.val())
+  })
+}
+
+export const watchUserLocation = (key) => {
+  const firebaseRef = firebase.database().ref()
+  const geoFire = new GeoFire(firebaseRef.child('geoData/'))
+  const options ={
+    enableHighAccuracy:false,
+    timeInterval:100000,
+    distanceInterval: 3000
+  }
+  Exponent.Location.watchPositionAsync(options,(pos)=>{
+    const {latitude,longitude} = pos.coords
+    geoFire.set(key, [latitude,longitude]).then(() => {
+        // console.log("Key has been added to GeoFire");
+      }, (error) => {
+        // console.log("Error: " + error);
+      })
+  })
+}
+export const watchUserLocationDemo = (key) => {
+  const firebaseRef = firebase.database().ref()
+  const geoFire = new GeoFire(firebaseRef.child('geoData/'))
+  const lat = -26.2041028
+  const lon = 28.0473051
+  geoFire.set(key, [lat, lon]).then(() => {
+      // console.log("Key has been added to GeoFire");
+    }, (error) => {
+      // console.log("Error: " + error);
+    })
+}
+
+export const findProfiles = (user, func) => {
+  const firebaseRef = firebase.database().ref()
+  const geoFire = new GeoFire(firebaseRef.child('geoData/'))
+  geoFire.get(user.uid).then(location => {
+    const geoQuery = geoFire.query({
+      center: location,
+      radius: user.maxDistance
+    })
+    geoQuery.on("ready", (res) => { // loaded geodata
+      geoQuery.cancel()
+    })
+    geoQuery.on("key_entered", (key, location, distance) => {
+      // console.log(key + " entered query at " + location + " (" + distance + " km from center)");
+      getUser(key).then(func)
+    })
+  })
+}
+
+const identifyMatches =  (liked, likedBack) => {
+  const likedArray = _.keys(liked).filter(x => liked[x])
+  const likedBackArray = _.keys(likedBack)
+  return _.intersection(likedBackArray,likedArray)
+}
+
+export const getMatches = (uid, func) => {
+  firebase.database().ref().child('relationships').child(uid).once('value', (snap) => {
+    if (snap.val()) {
+      const matches = identifyMatches(snap.val().liked, snap.val().likedBack)
+      func(matches)
+    }
+  })  
+}
+
+export const watchMatches = (uid, func) => {
+  firebase.database().ref().child('relationships').child(uid).on('value', (snap) => {
+    if (snap.val()) {
+      const matches = identifyMatches(snap.val().liked, snap.val().likedBack)
+      func(matches)
+    }
+  })
 }
--- app/screens/home.js
+++ app/screens/home.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {
+  Animated,
   StyleSheet,
   View,
   Image,
@@ -10,23 +11,56 @@ import {
 
 import Header from '../components/header';
 import {Router} from '../../app'
+import * as firebase from 'firebase'
+import * as FirebaseAPI from '../modules/firebaseAPI'
 
 const {height, width} = Dimensions.get('window');
 
 export default class Profile extends Component {
+  componentWillMount() {
+    this.state = { 
+      profileIndex: 0,
+      scrollEnabled: true,
+      profiles: [],
+      user: this.props.user,
+    }
+  }
+
+  componentDidMount() {
+    FirebaseAPI.watchUserLocationDemo(this.state.user.uid)
+    FirebaseAPI.watchUser(this.state.user.uid, (user) => {
+      if (user) {
+      this.setState({
+          user: user,
+          profileIndex: 0
+        })
+        FirebaseAPI.findProfiles(user, (profile) => {
+          const profiles = [...this.state.profiles, profile]
+          this.setState({profiles:profiles})  
+        })
+      }
+    })
+  }
+
   render() {
-    const {user} = this.props;
-    console.log("IN PROFILE: ")
-    console.log(user)
+    const {
+      profileIndex,
+      user,
+      profiles,
+    } = this.state
+
+
     return(
       <View style={{flex: 1}}>
         <TouchableOpacity onPress={() => {this.props.navigator.push(Router.getRoute('profile', {user}))}}>
           <Header facebookID={user.id} />
         </TouchableOpacity>
-        <View style={styles.container}>  
-          <Text style={styles.text}>
-            Welcome{'\n'}to{'\n'}DateGame!
-          </Text>
+        <View style={styles.container}> 
+          {profiles.slice(profileIndex,profileIndex + 3).reverse().map((profile, i, profileArray) => {
+            const fbImageUrl = `https://graph.facebook.com/${profile.id}/picture?height=${400}` // get facebook profile image url
+
+            return <Image resizeMode='cover' source={{uri: fbImageUrl}} style={{width: 150, height: 150}} />
+          })}
         </View>
       </View>
     )
@@ -35,7 +69,7 @@ export default class Profile extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 150,
+    marginTop: 20,
     height:height,
     width:width,
     backgroundColor:'white',
